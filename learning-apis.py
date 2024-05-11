a838de9bfb1ae6681d9708a69b675e4d935f7239
Francesco Castellano <92926676+Francesco118@users.noreply.github.com>
2024-05-11 15:27:11 -0600

    2.7 Lab

---
+++ learning-apis.py
@@ -0,0 +1,64 @@
+data = [
+    {
+        "type": "general",
+        "setup": "How do the trees get on the internet?",
+        "punchline": "They log on.",
+        "id": 121
+    },
+    {
+        "type": "general",
+        "setup": "What did the mountain climber name his son?",
+        "punchline": "Cliff.",
+        "id": 179
+    },
+    {
+        "type": "general",
+        "setup": "Did you hear the news?",
+        "punchline": "FedEx and UPS are merging. They’re going to go by the name Fed-Up from now on.",
+        "id": 100
+    },
+    {
+        "type": "programming",
+        "setup": "Lady: How do I spread love in this cruel world?",
+        "punchline": "Random Dude: [...💘]",
+        "id": 59
+    },
+    {
+        "type": "general",
+        "setup": "What’s 50 Cent’s name in Zimbabwe?",
+        "punchline": "200 Dollars.",
+        "id": 151
+    },
+    {
+        "type": "general",
+        "setup": "Have you heard of the band 1023MB?",
+        "punchline": "They haven't got a gig yet.",
+        "id": 111
+    },
+    {
+        "type": "general",
+        "setup": "Did you hear about the runner who was criticized?",
+        "punchline": "He just took it in stride",
+        "id": 94
+    },
+    {
+        "type": "general",
+        "setup": "Why does a chicken coop only have two doors?",
+        "punchline": "Because if it had four doors it would be a chicken sedan.",
+        "id": 362
+    },
+    {
+        "type": "general",
+        "setup": "I just watched a documentary about beavers.",
+        "punchline": "It was the best dam show I ever saw",
+        "id": 41
+    },
+    {
+        "type": "general",
+        "setup": "How do you fix a damaged jack-o-lantern?",
+        "punchline": "You use a pumpkin patch.",
+        "id": 124
+    }
+]
+
+print(data[3]['type'])
