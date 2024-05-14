9a923257bf4f11ed1db12cc04911a7cd06e80189
Francesco Castellano <92926676+Francesco118@users.noreply.github.com>
2024-05-14 12:09:44 -0600

    Testing quiz questions

---
+++ quiz-w4.py
@@ -0,0 +1,28 @@
+'''for v in "Ensign": 
+  print("Hello, World!")
+
+for j in range(len("Python")):
+  print(j)
+  
+for j in range(0,len("Python"),2):
+    print(j)  
+    
+x = 0
+while(x<5):
+    print("Hello, world!")
+    
+i = 0
+while (i < 10):
+  print(i)
+  i += 1
+  if(i == 6):
+    break
+    
+    '''
+j = 0;
+while (j < 10):
+  if (j == 4 or j == 6):
+    j += 1
+    continue
+  print(j)
+  j += 1
