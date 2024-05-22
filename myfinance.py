2c0d31c686c65e5ca0d0faf8a8eb8080cd41eaca
Francesco Castellano <92926676+Francesco118@users.noreply.github.com>
2024-05-22 17:52:46 -0600

    Developing Main

--- myfinance.py
+++ myfinance.py
@@ -9,13 +9,16 @@ print("Welcome to My Finance!")
 
 #Net Pay
 def netPay(hourlyWage, totalHours):
-        
+        hourlyWage = input(print("What is your hourly wage? "))
+        totalHours = input(print("How many hours did you work? "))
+        print("Your salary is: ", netPay)
         netPay = hourlyWage * totalHours
         return netPay
 
 #Enter Revenue or Expense
 def revenueOrExpense(revenueOrExpense):
-
+        print('Enter transaction name: ')
+        print('Enter amount (use negative sign for expense): ')
 #Show discretionary income
 def discretionaryIncome(netPay, revenueOrExpense):
 
@@ -24,8 +27,23 @@ def exitApplication():
     
 #The program itself
 def main():
-        
+        while exit == False:
+                print("Please select an option:")
+                print('1-Calculate net pay')
+                print('2-Enter revenue or expense')
+                print('3-Show discretionary income')
+                print('4-Exit the program')
+                userChoice = input(print("Please select an option:"))
 
-hourlyWage = input(print("What is your hourly wage? "))
-totalHours = input(print("How many hours did you work? "))
-print("Your salary is: ", netPay)
+                if userChoice == 1:
+                        netPay(hourlyWage, totalHours)
+                        print("Your salary is: ", netPay)
+                elif userChoice == 2:
+                        revenueOrExpense(revenueOrExpense)
+                elif userChoice == 3:
+                        discretionaryIncome(netPay, revenueOrExpense)
+                elif userChoice == 4:
+                        #exitApplication()
+                        break
+                else:
+                        print("Invalid option. Please try again.")
